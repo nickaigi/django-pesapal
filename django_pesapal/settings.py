59150e1a80838e21c07ae660f8001f6b2e861353
Nickson Kaigi <nickaigi@gmail.com>
2013-08-16 11:03:10 +0300

    setup template dirs

--- django_pesapal/settings.py
+++ django_pesapal/settings.py
@@ -118,6 +118,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(PROJECT_PATH, 'templates'),
 )
 
 INSTALLED_APPS = (
@@ -132,6 +133,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'south',
+    'core',
 )
 
 # A sample logging configuration. The only tangible logging
