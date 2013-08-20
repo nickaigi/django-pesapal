89b60fffe6d33879d4cf9f6b160fec880c715372
Nickson Kaigi <nickaigi@gmail.com>
2013-08-20 16:36:33 +0300

    more display fields

--- core/admin.py
+++ core/admin.py
@@ -2,8 +2,9 @@ from django.contrib import admin
 from core.models import Pesapal
 
 class PesapalAdmin(admin.ModelAdmin):
-    list_display = ('product_id',)
-    search_fields = ('product_id', )
+    list_display = ('user','product_id', 'status',)
+    search_fields = ('product_id', 'user',)
+    list_filter = ('status', )
 
 admin.site.register(Pesapal, PesapalAdmin)
 
