411fde9d6fa715cc6345d4f2baca7e951bdbf415
Nickson Kaigi <nickaigi@gmail.com>
2013-08-22 13:10:02 +0300

    cool test

--- core/tests.py
+++ core/tests.py
@@ -1,16 +1,62 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
+#!/usr/bin/env python
+import unittest
+import pesapal
+import requests
 
-Replace this with more appropriate tests for your application.
-"""
+KEY = 'uvzyNdMvjn6Ir4id+zwcUNT7bKOsp+wY'
+SECRET = 'fXFK6owbt2B00Yq6JscpvKmDm6o='
 
-from django.test import TestCase
+class TestPesapal(unittest.TestCase):
 
+    def setUp(self):
+        self.client = pesapal.PesaPal(KEY, SECRET, True)
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
+    def test_query_payment_status(self):
         """
-        Tests that 1 + 1 always equals 2.
+        determine the payment status
         """
-        self.assertEqual(1 + 1, 2)
+
+        client = self.client
+
+        params = {
+          'pesapal_merchant_reference': 'KE0C60008',
+          'pesapal_transaction_tracking_id': '8712e0bd-bbf1-4c50-a0a9-56b3019e7cc6'
+        }
+
+        pesapal_request = client.queryPaymentStatus(params)
+        print pesapal_request.to_url()
+        pesapal_response_data = requests.get(pesapal_request.to_url())
+        print pesapal_response_data.text
+
+    def test_query_payment_status_by_merchant_ref(self):
+
+        client = self.client
+
+        params = {
+          'pesapal_merchant_reference': 'KE0C60008'
+        }
+
+        pesapal_request = client.queryPaymentStatusByMerchantRef(params)
+        print pesapal_request.to_url()
+        pesapal_response_data = requests.get(pesapal_request.to_url())
+        print pesapal_response_data.text
+
+
+    def test_query_payment_details(self):
+
+        client = self.client
+
+        params = {
+          'pesapal_merchant_reference': '000',
+          'pesapal_transaction_tracking_id': '000'
+        }
+
+        pesapal_request = client.queryPaymentDetails(params)
+        print pesapal_request.to_url()
+        pesapal_response_data = requests.get(pesapal_request.to_url())
+        print pesapal_response_data.text
+
+
+
+if __name__ == '__main__':
+    unittest.main()
